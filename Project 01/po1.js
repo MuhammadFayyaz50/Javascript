6e8f6ab0934c1199fb0ef5105b472e2339d8bc3a
MuhammadFayyaz50 <77410571+MuhammadFayyaz50@users.noreply.github.com>
2021-01-16 22:45:58 +0500

    Add files via upload

---
+++ Project 01/po1.js
@@ -0,0 +1,19 @@
+
+const form = document.getElementById('form');
+const Username = document.getElementById('Username');
+const Email = document.getElementById('Email');
+const Password = document.getElementById('Password');
+const Password2 = document.getElementById('Password2');
+
+
+
+
+
+
+//add listener when submit//
+form.addEventListener('submit',function(e) {
+	e.preventDefault();
+	if (Username.value === '') {
+
+	}
+})
